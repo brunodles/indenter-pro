a632bc9009f25073f4105a27504c95373c212a26
Bruno <>
2013-09-03 12:12:20 -0300

    Classe "Indenter" criada. Adicionado método principal da classe "indent". Metodo para remoção dos espaços desnecessários. Método para encontrar os primeiros caracteres em branco e a quantidade. Testes simples, para verificar o funcionamento dos métodos criados.

---
+++ Desktop/src/indenterpro/Indenter.java
@@ -0,0 +1,37 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package indenterpro;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ *
+ * @author Bruno
+ */
+public class Indenter {
+
+    public String indent(String string) {
+        return string.replaceAll("[\\p{Blank}]++", " ");
+    }
+
+    public String firstsBlankCharacters(String string) {
+        Matcher matcher = Pattern.compile("([\\p{Blank}]++).*+").matcher(string);
+        if (matcher.matches()) {
+            return matcher.group(1);
+        }
+        return "";
+    }
+
+    public int countFirstsBlankCharacters(String string) {
+        return firstsBlankCharacters(string).length();
+    }
+
+    public static List<String> splitStringAsList(String string) {
+        return Arrays.asList(string.split("[\r\n]"));
+    }
+}
---
+++ Desktop/test/indenterpro/IndenterTest.java
@@ -0,0 +1,41 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package indenterpro;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Bruno
+ */
+public class IndenterTest {
+
+    Indenter indenter;
+
+    public IndenterTest() {
+        indenter = new Indenter();
+    }
+
+    @Test
+    public void testRemoveBlankCharacters() {
+        final String in = "String name   = \"temp\";";
+        final String outExpected = "String name = \"temp\";";
+        String out = indenter.indent(in);
+        assertEquals(outExpected, out);
+    }
+
+    @Test
+    public void testCountFirstsBlankCharacters() {
+        final String in = "    String name   = \"temp\";"; //4
+        assertEquals(4, indenter.countFirstsBlankCharacters(in));
+    }
+
+    @Test
+    public void testFirstsBlankCharacters() {
+        final String in = "        String name   = \"temp\";";
+        assertEquals("        ", indenter.firstsBlankCharacters(in));
+    }
+}
